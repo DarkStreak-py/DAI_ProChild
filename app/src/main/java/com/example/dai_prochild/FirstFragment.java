1aaea5636fbc974f28d628c997a388439d86a27c
mariacrdribeiro@gmail.com <CHugares6>
2021-03-30 17:55:40 +0100

    ecrãs do figma

--- app/src/main/java/com/example/dai_prochild/FirstFragment.java
+++ app/src/main/java/com/example/dai_prochild/FirstFragment.java
@@ -23,11 +23,11 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.textView5).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                        .navigate(R.id.action_FirstFragment_to_menu_Principal);
             }
         });
     }
---
+++ app/src/main/java/com/example/dai_prochild/Menu_Principal.java
@@ -0,0 +1,102 @@
+package com.example.dai_prochild;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+public class Menu_Principal extends Fragment {
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.menu_principal, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.btn_direitos).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(Menu_Principal.this)
+                        .navigate(R.id.action_menu_Principal_to_direitos);
+
+
+            }
+        });
+        view.findViewById(R.id.btnforum).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(Menu_Principal.this)
+                        .navigate(R.id.action_menu_Principal_to_forum4);
+
+
+            }
+        });
+        view.findViewById(R.id.duvidas_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(Menu_Principal.this)
+                        .navigate(R.id.action_menu_Principal_to_chat_Duvidas);
+
+
+            }
+        });
+        view.findViewById(R.id.btnapoio).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(Menu_Principal.this)
+                        .navigate(R.id.action_menu_Principal_to_linhaApoio);
+
+
+            }
+        });
+        view.findViewById(R.id.denuncia_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(Menu_Principal.this)
+                        .navigate(R.id.action_menu_Principal_to_denuncias);
+
+
+            }
+        });
+        view.findViewById(R.id.btn_materiais).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(Menu_Principal.this)
+                        .navigate(R.id.action_menu_Principal_to_materiais2);
+
+
+            }
+        });
+        view.findViewById(R.id.btnconta).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(Menu_Principal.this)
+                        .navigate(R.id.action_menu_Principal_to_perfil);
+
+
+            }
+        });
+        view.findViewById(R.id.btn_eventos).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(Menu_Principal.this)
+                        .navigate(R.id.action_menu_Principal_to_eventos);
+
+
+            }
+        });
+
+    }
+
+
+}
